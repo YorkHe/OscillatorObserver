5c5f7c605367fa11c2331b182c78c45fe571eb2c
York <kiddmagician@gmail.com>
2016-01-03 03:08:00 +0800

    set CUI

--- main.py
+++ main.py
@@ -31,6 +31,27 @@ class ThreadPool:
      def wait_completion(self):
           self.tasks.join()
 
+def output(x, y):
+     for _x, _y in zip(x,y):
+          outfile.write(str(_x) + '\t' + str(_y) + '\n')
+          outfile.close()
+
+print("==============================================================================")
+print("===                        Tektronix TDS1012B                              ===")
+print("===                        Oscillator Receiver                             ===")
+print("===                                                                        ===")
+print("===                                                                        ===")
+print("===                   Designed for Muon Detection Experiment               ===")
+print("===                                                                        ===")
+print("===                                                                        ===")
+print("===                                                            He Yu       ===")
+print("===                                                           2015-12      ===")
+print("==============================================================================")
+
+print("Program will start automatically.")
+
+print("Initiating...")
+
 device = TDS1012B()
 
 device.set_channel(1)
@@ -40,27 +61,30 @@ device.get_scale_parameters(device.channel1)
 total = 0
 start_time = datetime.datetime.now()
 
-pool = ThreadPool(4)
+pool = ThreadPool(3)
 
 i = 0
 
-def output(x, y):
-     for _x, _y in zip(x,y):
-          outfile.write(str(_x) + '\t' + str(_y) + '\n')
-          outfile.close()
+print("Initiation Done.")
+print("Connected to device:" + device.identity)
+print("Parameters:" + str(device.channel1))
+
+
+print("Data receiving starts.")
 
 try:
      device.write('data:source ch1')
      device.write('data:encdg ascii')
      while True:
           print("Now output file" + str(total) + ".")
-          outfile = open("data/0103/"+str(i)+".data", 'w+')
+          outfile = open("data/"+ datetime.date.today()[0]+"/"+str(i)+".data", 'w+')
           e = device.get_wave_form(device.channel1)
           x, y = device.wave.get_wave()
           pool.add_task(output, x, y)
           i = i + 1
 
 except KeyboardInterrupt:
+     pool.wait_completion()
      end_time = datetime.datetime.now()
      print "Program Interrupted."
      print "Time: " + str(end_time-start_time)
